c0841ce90d9970a156e8093332f31ac482b1076a
Kaustubh Bajpai <82926440+kaustubh-7@users.noreply.github.com>
2025-01-16 11:44:58 +0530

    Delete src directory

--- src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-export default function Input({label, ref, textArea, ...props}){
-    const classes = 'w-full p-1 border-b-2 border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus-border-stone-600'
-    return <p className='flex flex-col gap-1 my-4'>
-        <label className='text-sm font-bold uppercase text-stone-500'>{label}</label>
-        {textArea ? <textarea {...props} ref={ref} className={classes} />:<input ref={ref} className={classes} {...props} />}
-    </p>
-}
